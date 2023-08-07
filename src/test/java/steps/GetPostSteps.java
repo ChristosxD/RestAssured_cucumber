6a76c90e90df55db3c2a78cbd478d01d97af63cb
ChristosxD <christos.kotsoy@hotmail.gr>
2023-08-07 11:56:06 +0300

    added BBD class and implemented code at steps methods

--- src/test/java/steps/GetPostSteps.java
+++ src/test/java/steps/GetPostSteps.java
@@ -1,5 +1,30 @@
 package steps;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.is;
+
+
 public class GetPostSteps {
 
+    @Given("I perform GET operation for {string}")
+    public void iPerformGETOperationFor(String url) {
+        given().contentType(ContentType.JSON);
+    }
+
+    @And("I perform GET for the post number {string}")
+    public void iPerformGETForThePostNumber(String postNumber) {
+        BDDStyle.simpleGETPost(postNumber);
+    }
+
+    @Then("I should see the author name as {string}")
+    public void iShouldSeeTheAuthorNameAs(String arg0) {
+
+    }
 }
